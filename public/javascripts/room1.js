b172f6a00082b9dc5f20c2b07ed853c37e6b9b10
luwei0917 <luwei0917@gmail.com>
2014-03-04 19:09:22 -0500

    3rd version

--- public/javascripts/room1.js
+++ public/javascripts/room1.js
@@ -1,64 +1,127 @@
 $(document).ready(function(){
-    var convert = ['1','2','3','4','5','6','7','8','9','10','j','q','k'];
-    //-------------------------
-    //IMPORTANT
-    //I will give you a free lunch if you capture this sentence and send to me.
-    //If you don't do it by March 3rd. You have to give me a free dinner (I choose where to eat).
-    //This contract takes effect when it is uploaded online.
-    //Who do not abey this, is a lazy person.(I mean you ZNY).
 
-    (function( $ ){
-        $.fn.myfunction = function(context,ccc) {
-            //alert('hello world');
-            $('#servermsg').text(ccc);
 
-            //alert(value);
-            //alert(suit);
-            var suit = 'nothing';
-            var value = -1;
-            var divarray = [];
-            for(var i =0 ;i<context.length;i++){
-                var $temp = $('<div>',{id:"card"+i,class:"cards"});
-                //$($temp).attr('left','0');
-                //document.getElementById("card"+i).style.left = parseString((-50)*i)+"px";
-                $($temp).css('left',(i*15).toString()+"px");
-                //$($temp).attr('position','absolute');
+    var convert = ['X','A','2','3','4','5','6','7','8','9','10','J','Q','K'];
 
-                divarray.push($temp);
-                suit = context[i].suit;
-                value = context[i].value;
-                $temp.append(Poker.getCardImage(100,suit,convert[value]));
-                $('#overlay').append($temp);
+//
+//    (function( $ ){
+//        $.fn.myfunction = function(context,ccc) {
+//            //alert('hello world');
+//            $('#servermsg').text(ccc);
+//
+//            //alert(value);
+//            //alert(suit);
+//            var suit = 'nothing';
+//            var value = -1;
+//            //var divarray = [];
+//            for(var i =0 ;i<context.length;i++){
+//                var $temp = $('<div>',{id:i.toString(), class:"cards"});
+//                //$($temp).attr('left','0');
+//                //document.getElementById("card"+i).style.left = parseString((-50)*i)+"px";
+//                $($temp).css('left',(i*15).toString()+"px");
+//                //$($temp).attr('position','absolute');
+//
+//                //divarray.push($temp);
+//                //ALL_SUIT =['spades','hearts','diamonds','clubs','jokers'];
+//                if(context[i].suit === "jokers"){
+//                    if(context[i].value === 1){
+//                        suit = 's' //little joker
+//                        value = 'JOKER';
+//                    }
+//                    else{
+//                        suit = 'h'; //big joker
+//                        value= 'JOKER';
+//                    }
+//                }
+//                else{
+//                    suit = context[i].suit;
+//                    value = convert[context[i].value];
+//                }
+//
+//                $temp.append(Poker.getCardImage(100,suit,value));
+//                $('#overlay').append($temp);
+//            }
+//            $('.cards').click(function(){
+//                //alert(this.id);
+//                if ($(this).hasClass('select')){
+//                    $(this).animate({top:'+=20px'},1);
+//                    $(this).removeClass('select')
+//                }else{
+//                    $(this).animate({top:'-=20px'},1);
+//                    $(this).addClass('select')
+//                }
+//            });
+//            return this;
+//        };
+//    })( jQuery );
+//
+
+    $.fn.myfunction = function(context,ccc) {
+        //alert('hello world');
+        $('#servermsg').text(ccc);
+
+        //alert(value);
+        //alert(suit);
+        var suit = 'nothing';
+        var value = -1;
+        //var divarray = [];
+        for(var i =0 ;i<context.length;i++){
+            var $temp = $('<div>',{id:i.toString(), class:"cards"});
+            //$($temp).attr('left','0');
+            //document.getElementById("card"+i).style.left = parseString((-50)*i)+"px";
+            $($temp).css('left',(i*15).toString()+"px");
+            //$($temp).attr('position','absolute');
+
+            //divarray.push($temp);
+            //ALL_SUIT =['spades','hearts','diamonds','clubs','jokers'];
+            if(context[i].suit === "jokers"){
+                if(context[i].value === 1){
+                    suit = 's' //little joker
+                    value = 'JOKER';
+                }
+                else{
+                    suit = 'h'; //big joker
+                    value= 'JOKER';
+                }
             }
-            return this;
-        };
-    })( jQuery );
+            else{
+                suit = context[i].suit;
+                value = convert[context[i].value];
+            }
 
-
+            $temp.append(Poker.getCardImage(100,suit,value));
+            $('#overlay').append($temp);
+        }
+        $('.cards').click(function(){
+            //alert(this.id);
+            if ($(this).hasClass('select')){
+                $(this).animate({top:'+=20px'},1);
+                $(this).removeClass('select')
+            }else{
+                $(this).animate({top:'-=20px'},1);
+                $(this).addClass('select')
+            }
+        });
+        return this;
+    };
 
 
     connect_to_server();
-    var $div1 = $('<div>',{id:"card1"});
-    var $div2 = $('<div>',{id:"card2"});
 
-
-    $("div").click(function(){
-        alert('ID:'+this.id);
-    });
 
 //    $(".cards").each(function(){
 //        this.click(function(){
 //            alert("ID: "+this.id);
 //        });
 //    });
-        //$('#showcard').text('got a Heart Queen!');
-        //if (this.hasClass('select')){
-        //    this.animate({top:'+=20px'},'fast');
-        //    this.removeClass('select')
-        //}else{
-        //    this.animate({top:'-=20px'},'fast');
-        //    this.addClass('select')
-        //}
+    //$('#showcard').text('got a Heart Queen!');
+    //if (this.hasClass('select')){
+    //    this.animate({top:'+=20px'},'fast');
+    //    this.removeClass('select')
+    //}else{
+    //    this.animate({top:'-=20px'},'fast');
+    //    this.addClass('select')
+    //}
 //
 //    });
 //
@@ -83,19 +146,18 @@ $(document).ready(function(){
 //    canvas.drawPokerCard(0,0,100,'hearts','6');
 //    canvas.drawPokerCard(20,0,100);
 
-    $div1.append(Poker.getCardImage(100,'hearts','q'));
-    $div2.append(Poker.getCardImage(100,'d','k'));
 
 //    $('#overlay').append($div1);
 //    $('#overlay').append($div2);
-
-    $('#canvas').click(function(){
-        $div1.removeClass('select');
-        $div1.css('top',0);
-        $div1.remove();
-    })
-    $('#showcard').click(function(){
-        $('#overlay').append($div1);
-    })
+//
+//    $('#canvas').click(function(){
+//        $div1.removeClass('select');
+//        $div1.css('top',0);
+//        $div1.remove();
+//    })
+//
+//    $('#showcard').click(function(){
+//        $('#overlay').append($div1);
+//    })
 
 })
